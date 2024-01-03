d30761517db7ec051e2ea72f59f52102acbffa6b
Dawid Dzitko <3671749@teb-gdansk.ad>
2024-01-03 11:44:05 +0100

    Stworzylem klase person

---
+++ obiektowanie/Program.cs
@@ -0,0 +1,11 @@
+﻿using obiektowanie;
+
+class Program
+{
+    static void Main()
+    {
+        // Przykład użycia
+        person person1 = new person("Michal", "WielkiK", "Mężczyzna", new DateTime(1990, 1, 14));
+        Console.WriteLine(person1);
+    }
+}
---
+++ obiektowanie/person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obiektowanie
+{
+     class person
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Plec { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+        // Konstruktor
+        public person(string imie, string nazwisko, string plec, DateTime dataUrodzenia)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Plec = plec;
+            DataUrodzenia = dataUrodzenia;
+        }
+
+        // Metoda do wyświetlania informacji o osobie
+        public override string ToString()
+        {
+            return $"person: {Imie} {Nazwisko}\nPłeć: {Plec}\nData urodzenia: {DataUrodzenia.ToShortDateString()}";
+        }
+    }
+}
